b337d6b1983bb44d3c64349fb270d390af245f25
hubertleszcz <hubciol1308@gmail.com>
2024-03-14 11:26:46 +0100

    quick fixes

--- plot_circle_areas.m
+++ plot_circle_areas.m
@@ -1,9 +1,8 @@
 function plot_circle_areas(circle_areas)
-   
     plot(circle_areas)
     xlabel("Indeks koła");
     ylabel("Suma pól");
     title("Wartość circle areas");
 
- %print -dpng zadanie3.png 
+ print -dpng zadanie3.png 
 end
